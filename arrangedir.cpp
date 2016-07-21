160ff06998fe8f32b60e6e1a20122b05cc553d87
Carlos Ortega <carlitosortegah@live.com>
2016-07-20 23:59:48 -0700

    After so long, by asking in SO, I got to finish my program
    Finished and better than never, the arrange-dir program arranges you messy files for less than a cent. The problem I was facing was one that I would never have thought about. I asked in stackoverflow.com and got a great answer. My problem was that (did not look like -specially from a java background-, and was like half an hour like "what?", but) a pointer was pointing to nothing. That is why pointers are evil!
    Then I had another problem. A HUGE PROBLEM. But I solved it at the end. The problem was that I just wrote some stupid thing instead of what I needed. But the reason it was a huge problem was that the program gave no warnings, no errors, and behaved extremely "undefined". If I ran the program from the command line I got different output than running it by making click and also got different output by running it from Visual Studio, and worse things like giving different outputs by running in the same conditions (which I did dozens of times). 
    I don't understand how can people say Visual Studio is the best IDE when it gives me max_size instead of size if I type length, as vector properties/functions. I prefer IntelliJ IDEA. Got a new computer, that was why I resumed this program dev.

--- arrangedir.cpp
+++ arrangedir.cpp
@@ -1,60 +1,104 @@
-#include "stdafx.h"
+
+/* Hello, I am the preprocessors train! */
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
+#include <Windows.h>
 #include "string"
 #include "shlwapi.h"
-#include "arrangedir.h"
 #include <direct.h>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <windows.h>
-
 #pragma comment(lib, "Shlwapi.lib")
 
 using namespace std;
 
-static vector<wstring> extensions;
-static vector<wstring> files;
-static wstring path;
-static DWORD err;
+class ArrangeDir {
 
-void defPathName() {
-	LPWSTR  buf = new wchar_t;
-	GetModuleFileName(NULL, buf, MAX_PATH);
-	path = (wstring) buf;
-	path = path.substr(0, path.find_last_of(L"/\\"));
-}
+public:
 
-void defFileNames() {
-	HANDLE hFind=0;
-	WIN32_FIND_DATA data;
-	wcout << path << endl;
-	//LPCWSTR s = (path + L"\\*.*").c_str();
-	wstring s = (path + L"\\*.*");
-	hFind = FindFirstFile(s.c_str(), &data);
-	cout << "is hFind invalid?" << endl;
-	if (hFind != INVALID_HANDLE_VALUE) {
-		cout << "No! congratuleishons" << endl;
-		//foreach match
-		do {
+	vector<wstring> extensions;
+	vector<wstring> files;
+	wstring path;
+	DWORD err;
+	int fails = 0;
+
+	ArrangeDir(wstring dir = L"here-warn") {
+
+		try {
+			if (dir != L"here-warn" && dir != L"here") {
+				path = wstring(dir);
+			}
+			else if (dir == L"here-warn"){
+				cout << "Warning, close the program if you don't want to mess with this directory. This could break your computer if running from the wrong directory. Otherwise write start." << endl;
+				string s;
+				cin >> s;
+				if (s == "start") {
+					defPathName();
+				}
+				else exit(0);
+			}
+			else if (dir == L"here") {
+				defPathName();
+			}
+			defFileNames();
+			defExtensions();
+			classifyFiles();
+			cout << "Fails: " << fails << "\n" << "\n";
+		}
+		catch (std::out_of_range ofr) {
+			if (err != 0L) {
+				cout << "error code: " << err << "\nRefer to https://msdn.microsoft.com/en-us/library/windows/desktop/ms681382(v=vs.85).aspx \n";
+				system("pause");
+			}
+			else {
+				cout << "err 0 !" << endl;
+				classifyFiles();
+			}
+		}
+		
+		//process end
+		system("pause");
+
+	}
+
+	void defPathName() {
+		wchar_t buf[MAX_PATH];
+		GetModuleFileNameW(NULL, buf, MAX_PATH);
+		path = wstring(buf);
+		path = path.substr(0, path.find_last_of(L"/\\"));
+	}
+
+	void defFileNames() {
+		HANDLE hFind = 0;
+		WIN32_FIND_DATA data;
+		hFind = FindFirstFile((path + L"\\*.*").c_str(), &data);
+		if (hFind != INVALID_HANDLE_VALUE) {
+			//foreach match
+			do {
 				wstring fn = data.cFileName;
 				if (data.cFileName != L"" && data.cFileName != L"." && data.cFileName != L".."
 					&& fn.find_last_of(L".") != wstring::npos) {
-					wcout << "-" << endl << data.cFileName << endl;
 					files.push_back(data.cFileName);
 				}
-		} while (FindNextFile(hFind, &data));
+			} while (FindNextFile(hFind, &data));
+		}
+		else { //If hFind == invalid handle
+			cout << "Fail." << endl;
+			err = GetLastError();
+		}
+		FindClose(hFind);
 	}
-	else{ //If hFind == invalid handle
-		cout << "Fail." << endl;
-		err = GetLastError();
-	}
-	FindClose(hFind);
-}
 
-void defExtensions() {
-	//foreach file
+	void defExtensions() {
 		for (wstring fileName : files) {
-			string fn = string(fileName.begin(), fileName.end());
 			if (fileName.find_last_of(L".") != wstring::npos) {
 				wstring sub = fileName.substr(fileName.find_last_of(L"."));
 				sub = sub.wstring::substr(1);
@@ -62,68 +106,55 @@ void defExtensions() {
 			}
 			else { cout << "npos" << endl; }
 		}
-}
+	}
 
-BOOL DirectoryExists(LPCWSTR szPath)
-{
-	DWORD dwAttrib = GetFileAttributes(szPath);
-	return (dwAttrib != INVALID_FILE_ATTRIBUTES &&
-		(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
-}
+	BOOL DirectoryExists(LPCWSTR szPath)
+	{
+		DWORD dwAttrib = GetFileAttributes(szPath);
+		return (dwAttrib != INVALID_FILE_ATTRIBUTES &&
+			(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+	}
 
-void classifyFiles() {
-	for (vector<wstring>::size_type i=0; i < files.max_size(); i++) {
-		//if each folder doesn't exist, create folder
-		try {
-			if (extensions.at(i) != L"." && extensions.at(i) != L"..") {
-				LPCWSTR folderName = (path + L"\\" + extensions.at(i)).c_str();
-				cout << endl;
-				//if (!DirectoryExists(folderName)) {
-					//monster
+	void classifyFiles() {
+		for (vector<wstring>::size_type i = 0; i < files.max_size(); i++) {
+			//if each folder doesn't exist, create folder
+			try {
+				if (extensions.at(i).length() != 0) {
+					LPCWSTR folderName = (path + L"\\" + extensions.at(i)).c_str();
+					cout << endl;
 					_mkdir(
-					(string(path.begin(), path.end()) + "\\" + string(extensions.at(i).begin(), extensions.at(i).end())).c_str()
+						(string(path.begin(), path.end()) + "\\" + string(extensions.at(i).begin(), extensions.at(i).end())).c_str()
 					);
-				//};
-				//cout << "Directory boolean: " << DirectoryExists(folderName) << endl;
-				LPCWSTR d = (path + L"\\" + extensions.at(i) + L"\\" + files.at(i)).c_str();
-				wcout << L"Destination: " << path + L"\\" + extensions.at(i) << endl;
-				//move file
-				BOOL b = MoveFile((path + L"\\" + extensions.at(i) + L"\\" + files.at(i)).c_str(), d);
-				if (b == FALSE) {
-					wcout << files.at(i) << L" was NOT moved to " << L" ... " << L"\\" + extensions.at(i) + L"\\" +files.at(i) + L"."<< endl;
-					cout<< GetLastError() << endl;
+					wstring o = path + L"\\" + files.at(i);
+					wstring d = path + L"\\" + extensions.at(i) + L"\\" + files.at(i);
+					//move file
+					wcout << L"Moving " << endl << path + L"\\" + files.at(i) << endl << L" to " << endl << path + L"\\" + extensions.at(i) + L"\\" + files.at(i) << endl;
+					BOOL b = MoveFile(o.wstring::c_str(), d.wstring::c_str());
+					if (b == FALSE) {
+						wcout << "FAIL MOVING " << files.at(i) << endl;
+						cout << "Error code: " << GetLastError() << endl;
+						fails++;
+					}
+					else {
+						wcout << L"SUCCESS MOVING " << files.at(i) << endl;
+					}
+					cout << endl;
 				}
-				else {
-					wcout << L"Moved " << files.at(i) << L" to " << ( L" ... \\" + extensions.at(i) + L"\\" + files.at(i)) << endl;
-				}
-				cout << endl;
 			}
-		}catch (std::out_of_range oor_E) {
-			return; //I don't know why does this happen but it seems this is the only solution
+			catch (std::out_of_range oor_E) {
+				return;
+			}
 		}
 	}
-}
+};
 
-int main()
+int main(int argc, char* argv[])
 {
-	try {
-		//defPathName();
-		path = L"C:\\Users\\Zorudeam\\Desktop\\Carpeta\\G";
-		defFileNames();
-		defExtensions();
-		classifyFiles();
+	wstring dir = L"here-warn";
+	if (argc > 1) {
+		string s = argv[1];
+		dir = wstring(s.begin(), s.end());
 	}
-	catch (std::out_of_range ofr) { 
-		if(err != 0L){
-			cout << "error code: " << err << "\nRefer to https://msdn.microsoft.com/en-us/library/windows/desktop/ms681382(v=vs.85).aspx \n";
-			system("pause"); return -2; 
-		}
-		else {
-			cout << "err 0 !" << endl;
-			classifyFiles();
-		}
-	}
-
-	system("pause");
+	ArrangeDir process1 (dir);
 	return 0;
-}
+};
